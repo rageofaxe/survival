0ccdfa466b75b1285cb2dba0693443407b694ddd
rageofaxe <rageofaxe@gmail.com>
2024-09-19 02:42:53 -0700

    update

--- src/routes/game/01-easy-walk/+page.svelte
+++ src/routes/game/01-easy-walk/+page.svelte
@@ -6,17 +6,28 @@
 	import ActionBar from '$lib/components/ActionBar.svelte';
 	
 	import Ascii from '$lib/components/Ascii.svelte';
+	import { addAction, getActionBasket, getAreaActions, getAreaDescription, getItemBasket, getItemsBasketName, getResources, getTime } from '../../../utils/helper';
+	import { mission, player } from '$lib/current-game/game';
+
+	let time = getTime(mission);
+	let resources = getResources(mission);
+	let areaDescription = getAreaDescription(mission, player)
+	let itemBasket = getItemBasket(player)
+	let itemsBasketName = getItemsBasketName(player)
+	let actionBasket = getActionBasket(player)
+	let addPlayerAction = addAction(player)
+	let areaActions = getAreaActions(mission)
 </script>
 
 <div class="container">
 	<div class="flex">
-		<StatusBar />
+		<StatusBar {resources} {time} />
 		<div class="row">
 			<Ascii />
-			<Description />
+			<Description {areaDescription} />
 		</div>
 		<div class="row">
-			<Basket />
+			<Basket {itemBasket} {itemsBasketName} />
 			<ActionBar />
 		</div>
 		<LoopController />
